7b46773a868aedf35d7e54a7b6898136ba061ab4
Priyank Pulumati <pulumati.priyank@gmail.com>
2017-05-26 14:38:14 +0530

    changed wrecker file and envscript

--- scripts/app_env_vars.sh
+++ scripts/app_env_vars.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 cat > .env << EOF
-  export bc_intranet_client_id=${BC_CLIENT_ID}
-  export bc_intranet_client_secret=${BC_CLIENT_SECRET}
-  export bc_app_key=${BC_APP_KEY}
-  export bc_env=${BC_ENV}
-  export bc_host=${BC_LOCALHOST}
-  export bc_mongo_db="${MS_NAME}db"
-  export PORT=${BC_PORT}
-  export MONGO_URI=${BC_MONGO_URI}
+  bc_intranet_client_id=${BC_CLIENT_ID}
+  bc_intranet_client_secret=${BC_CLIENT_SECRET}
+  bc_app_key=${BC_APP_KEY}
+  bc_env=${BC_ENV}
+  bc_host=${BC_LOCALHOST}
+  bc_mongo_db="${MS_NAME}"
+  PORT=${BC_PORT}
+  MONGO_URI=${BC_MONGO_URI}
 EOF
